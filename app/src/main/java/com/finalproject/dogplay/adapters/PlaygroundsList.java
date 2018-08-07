62b7d763b91e0d5e983ec9aeeac2a0099e220beb
Ruslan1abramov <ruslan1abramov@gmail.com>
2018-08-07 12:29:08 +0300

    code refactoring

---
+++ app/src/main/java/com/finalproject/dogplay/adapters/PlaygroundsList.java
@@ -0,0 +1,60 @@
+package com.finalproject.dogplay.adapters;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.finalproject.dogplay.R;
+import com.finalproject.dogplay.models.Playground;
+import com.finalproject.dogplay.models.UserProfile;
+
+import java.util.List;
+
+public class PlaygroundsList extends ArrayAdapter<Playground> {
+
+    private Activity context;
+    private List<Playground> playgrounds;
+
+    public PlaygroundsList(Activity context, List<Playground> playgroundsList) {
+        super(context, R.layout.playgrounds_listview_layout, playgroundsList);
+        this.context = context;
+        this.playgrounds = playgroundsList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+
+        View listViewItem = inflater.inflate(R.layout.playgrounds_listview_layout, null, true);
+
+        TextView playgroundName     = listViewItem.findViewById(R.id.playgroundName_textView);
+        TextView playgroundUsers    = listViewItem.findViewById(R.id.playgroundUsers_textView);
+
+        Playground playground = playgrounds.get(position);
+
+        playgroundName.setText(playground.getAddress());
+
+        String users;
+        if (!playground.getUsers().isEmpty()) {
+            StringBuilder userBuilder = new StringBuilder();
+            for (UserProfile userProfile : playground.getUsers()) {
+                userBuilder.append(userProfile.getuName()).append(" & ")
+                        .append(userProfile.getdName()).append("; ");
+            }
+            users = userBuilder.toString();
+        } else {
+            users = " There are'nt any users currently at this playground";
+        }
+
+        playgroundUsers.setText(users);
+
+
+        return listViewItem;
+    }
+
+}
