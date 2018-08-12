5ae118421189b0b954fe34ab5b2e9229d09ab313
Ruslan1abramov <ruslan1abramov@gmail.com>
2018-08-12 11:54:07 +0300

    fixed map fragment refresh

--- app/src/main/java/com/finalproject/dogplay/SearchDogParkActivity.java
+++ app/src/main/java/com/finalproject/dogplay/SearchDogParkActivity.java
@@ -1,8 +1,6 @@
 package com.finalproject.dogplay;
 
 import android.content.Intent;
-import android.os.Handler;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -23,9 +21,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Objects;
+
 
 public class SearchDogParkActivity extends AppCompatActivity {
 
